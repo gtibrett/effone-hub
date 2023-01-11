ed385dd7d9afca19880507acb7fa98cd4784a139
Brett Harris <gtibrett@gmail.com>
2023-01-11 14:46:47 -0500

    responsive updates

--- src/standings/Drivers.tsx
+++ src/standings/Drivers.tsx
@@ -23,7 +23,7 @@ export default function Drivers() {
 	
 	useEffect(() => {
 		if (season) {
-			const dataUrl                   = getAPIUrl(`/${season}/driverStandings.json`);
+			const dataUrl = getAPIUrl(`/${season}/driverStandings.json`);
 			Caxios.get<Responses['DriverStandingsByYearResponse']>(dataUrl)
 			      .then(mapDriversStandings)
 			      .then(data => setStandings(data))
@@ -39,43 +39,52 @@ export default function Drivers() {
 	
 	return (
 		<Grid container spacing={2} alignItems="stretch">
-			<Grid item xs={5}>
+			<Grid item xs={12} lg={5}>
 				<Place driverId={p1.Driver?.driverId} place={1} points={p1.points}/>
 				<Divider/>
 				<Place driverId={p2.Driver?.driverId} place={2} points={p2.points}/>
 				<Divider/>
 				<Place driverId={p3.Driver?.driverId} place={3} points={p3.points}/>
 			</Grid>
-			<Grid item xs={7} >
-				<Box pr={2} height="calc(100% - 8px)">
-				<DataGrid
-					sx={sx}
-					rows={rest}
-					density="compact"
-					columns={
-						[
-							{
-								field: 'position',
-								headerName: 'P',
-								headerAlign: 'center',
-								type: 'number',
-								align: 'center',
-								width: 16
-							},
-							{
-								field: 'code',
-								headerName: 'Driver',
-								flex: 1,
-								renderCell: ({row}) => <ByLine id={row.Driver?.driverId} avatarProps={{size: 24}} flagProps={{size: 16}}/>
-							},
-							{
-								field: 'points',
-								headerName: 'Points',
-								type: 'number'
-							}
-						]
+			<Grid item xs={12} lg={7}>
+				<Box sx={{
+					height: {
+						xs: 300,
+						lg: 'calc(100% - 8px)'
+					},
+					pr: {
+						xs: 0,
+						lg: 4
 					}
-				/>
+				}}>
+					<DataGrid
+						sx={sx}
+						rows={rest}
+						density="compact"
+						columns={
+							[
+								{
+									field: 'position',
+									headerName: 'P',
+									headerAlign: 'center',
+									type: 'number',
+									align: 'center',
+									width: 16
+								},
+								{
+									field: 'code',
+									headerName: 'Driver',
+									flex: 1,
+									renderCell: ({row}) => <ByLine id={row.Driver?.driverId} avatarProps={{size: 24}} flagProps={{size: 16}}/>
+								},
+								{
+									field: 'points',
+									headerName: 'Points',
+									type: 'number'
+								}
+							]
+						}
+					/>
 				</Box>
 			</Grid>
 		</Grid>
