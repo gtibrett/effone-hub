e7d02dc2ce4d0cbdb0d7813e5a9c3431d2e46116
Brett Harris <gtibrett@gmail.com>
2023-01-13 18:37:24 -0500

    add lap times data table

---
+++ src/race/lapTimes/LapTimesTable.tsx
@@ -0,0 +1,61 @@
+import {Box} from '@mui/material';
+import {DataGrid, GridColDef} from '@mui/x-data-grid';
+import ByLine from '../../drivers/ByLine';
+import {LapTimesProps} from './LapTimes';
+import useLapTimeChartData, {LapChartSeries} from './useLapTimeChartData';
+
+type LapTimesTableRow = {
+	driverId: LapChartSeries['id'];
+	laps: {
+		[lap: string]: string | null
+	};
+}
+
+export default function LapTimesTable({laps, results}: LapTimesProps) {
+	const flatData: LapTimesTableRow[] = [];
+	const data                         = useLapTimeChartData(laps, results);
+	
+	data.forEach((serie) => {
+		flatData.push({
+			driverId: serie.id,
+			laps: Object.fromEntries(serie.data.map((d) => (
+				[`lap_${d.x}`, d.timing.time]
+			)))
+		});
+	});
+	
+	const columns: GridColDef<LapTimesTableRow>[] = [
+		{
+			field: 'driverId',
+			headerName: 'Driver',
+			flex: 1,
+			renderCell: ({value}) => (
+				<ByLine id={value} variant="full"/>
+			),
+			minWidth: 240
+		}
+	];
+	
+	for (let i = 1; i <= laps.length; i++) {
+		columns.push(
+			{
+				field: `lap_${i}`,
+				headerName: String(i),
+				type: 'dateTime',
+				align: 'center',
+				headerAlign: 'center',
+				width: 100,
+				valueGetter: ({row, field}) => {
+					console.log(row, field);
+					return row.laps[field];
+				}
+			}
+		);
+	}
+	
+	return (
+		<Box height={800}>
+			<DataGrid columns={columns} rows={flatData} getRowId={r => r.driverId}/>
+		</Box>
+	);
+}
