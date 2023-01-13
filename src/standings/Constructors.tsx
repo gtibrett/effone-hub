ef651c486ca36358fb131745fcebce71cff3c41a
Brett Harris <gtibrett@gmail.com>
2023-01-13 15:28:31 -0500

    remove DataTable remove Breakpoints helper component

--- src/standings/Constructors.tsx
+++ src/standings/Constructors.tsx
@@ -1,8 +1,11 @@
-import {GridColDef} from '@mui/x-data-grid';
+import {Alert, Skeleton} from '@mui/material';
+import {DataGrid, GridColDef} from '@mui/x-data-grid';
+import {useEffect, useState} from 'react';
+import Caxios from '../api/Caxios';
 import {getAPIUrl, mapConstructorsStandings} from '../api/Ergast';
 import {useAppState} from '../app/AppStateProvider';
 import ByLine from '../constructors/ByLine';
-import DataTable from '../ui-components/DataTable';
+import {Responses, Standing} from '../types/ergast';
 
 const sx = {
 	border: 0,
@@ -12,15 +15,30 @@ const sx = {
 };
 
 export default function Constructors() {
-	const [{season}] = useAppState();
-	const dataUrl    = getAPIUrl(`/${season}/constructorStandings.json`);
+	const [{season}]      = useAppState();
+	const [data, setData] = useState<Standing[] | undefined>();
+	
+	useEffect(() => {
+		if (!data) {
+			const dataUrl = getAPIUrl(`/${season}/constructorStandings.json`);
+			Caxios.get<Responses['ConstructorStandingsByYearResponse']>(dataUrl)
+			      .then(mapConstructorsStandings)
+			      .then((results) => setData(results));
+		}
+	});
+	
+	if (!data) {
+		return <Skeleton variant="rectangular" height={400}/>;
+	}
+	
+	if (!data.length) {
+		return <Alert variant="outlined" severity="info">Constructor Standings Data Not Available</Alert>;
+	}
 	
 	return (
-		<DataTable
+		<DataGrid
 			sx={sx}
-			dataUrl={dataUrl}
-			mapper={mapConstructorsStandings}
-			cacheFor={60 * 60 * 8}
+			rows={data}
 			autoHeight
 			density="compact"
 			pageSize={10}
@@ -30,14 +48,14 @@ export default function Constructors() {
 						field: 'code',
 						headerName: 'Constructor',
 						flex: 1,
-						renderCell: ({row}) => <ByLine id={row.Constructor.constructorId} variant="link"/>
+						renderCell: ({row}) => <ByLine id={row.Constructor?.constructorId} variant="link"/>
 					},
 					{
 						field: 'points',
 						headerName: 'Points',
 						type: 'number'
 					}
-				] as GridColDef[]
+				] as GridColDef<Standing>[]
 			}
 		/>
 	);
