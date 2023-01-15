246ee3e0d258fa486facaeca33ca30316d72ef37
Brett Harris <gtibrett@gmail.com>
2023-01-15 17:22:36 -0500

    responsive header

--- src/ui-components/Header.tsx
+++ src/ui-components/Header.tsx
@@ -1,6 +1,6 @@
 import {faFlagCheckered} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {AppBar, Grid, Toolbar, Typography, useTheme} from '@mui/material';
+import {AppBar, Grid, Hidden, Toolbar, Tooltip, Typography, useTheme} from '@mui/material';
 import SeasonMenu from '../schedule/SeasonMenu';
 import Link from './Link';
 
@@ -13,10 +13,27 @@ export default function Header() {
 				<AppBar color="primary" sx={{background: theme.palette.primary.dark}}>
 					<Toolbar>
 						<Grid container spacing={2} alignItems="center">
-							<Grid item><FontAwesomeIcon icon={faFlagCheckered} size="3x" aria-hidden/></Grid>
-							<Grid item>
-								<Link to="/" color="inherit" sx={{textDecoration: 'none'}}><Typography variant="h3" component="h1" sx={{fontFamily: 'Racing Sans One'}}>effOne Hub</Typography></Link>
-							</Grid>
+							<Hidden mdUp>
+								<Grid item>
+									<Link to="/" color="inherit" sx={{textDecoration: 'none'}}>
+										<Tooltip title="effOne Hub" placement="right">
+											<FontAwesomeIcon icon={faFlagCheckered} size="3x"/>
+										</Tooltip>
+									</Link>
+								</Grid>
+							</Hidden>
+							<Hidden mdDown>
+								<Grid item>
+									<Link to="/" color="inherit" sx={{textDecoration: 'none'}}>
+										<FontAwesomeIcon icon={faFlagCheckered} size="3x" aria-hidden/>
+									</Link>
+								</Grid>
+								<Grid item>
+									<Link to="/" color="inherit" sx={{textDecoration: 'none'}}>
+										<Typography variant="h3" component="h1" sx={{fontFamily: 'Racing Sans One'}}>effOne Hub</Typography>
+									</Link>
+								</Grid>
+							</Hidden>
 							<Grid item xs/>
 							<Grid item><SeasonMenu/></Grid>
 						</Grid>
