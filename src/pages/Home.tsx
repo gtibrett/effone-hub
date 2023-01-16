651ed8d9ce0841aa5e3ba9f0b1c600b8d5f8b4f5
Brett Harris <gtibrett@gmail.com>
2023-01-16 00:29:37 -0500

    add driver's standings chart

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import {Card, CardHeader, Grid} from '@mui/material';
 import Schedule from '../schedule/Schedule';
 import Constructors from '../standings/Constructors';
 import Drivers from '../standings/Drivers';
+import DriversChart from '../standings/DriversChart';
 
 export default function Home() {
 	return (
@@ -17,6 +18,7 @@ export default function Home() {
 					<Grid item xs={12}>
 						<Card variant="outlined">
 							<CardHeader title="Driver's Standings"/>
+							<DriversChart/>
 							<Drivers/>
 						</Card>
 					</Grid>
