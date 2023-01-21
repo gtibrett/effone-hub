8868da3262659c36133ad651a9033d8e80f055df
Brett Harris <gtibrett@gmail.com>
2023-01-20 23:38:11 -0500

    refined wikipedia links

---
+++ src/ui-components/citations/WikipediaLink.tsx
@@ -0,0 +1,16 @@
+import {faWikipediaW} from '@fortawesome/free-brands-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {LinkProps, Typography} from '@mui/material';
+import {visuallyHidden} from '@mui/utils';
+import Link from '../Link';
+
+
+export default function WikipediaLink({href}: Pick<LinkProps, 'href'>) {
+	
+	return (
+		<Link href={href} target="_blank" to="">
+			<Typography variant="caption"><FontAwesomeIcon icon={faWikipediaW}/> more on wikipedia</Typography>
+			<Typography sx={visuallyHidden}> (opens in a new window)</Typography>
+		</Link>
+	)
+}
