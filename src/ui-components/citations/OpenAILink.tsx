b9d67002a99ad3e84025889273d9ea12b18fa842
Brett Harris <gtibrett@gmail.com>
2023-01-20 23:39:12 -0500

    incorporate content generated by OpenAI until ported to a new supplementary content API

---
+++ src/ui-components/citations/OpenAILink.tsx
@@ -0,0 +1,15 @@
+import {faRobot} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {Typography} from '@mui/material';
+import {visuallyHidden} from '@mui/utils';
+import Link from '../Link';
+
+export default function OpenAILink() {
+	
+	return (
+		<Link href="https://openai.com" target="_blank" to="">
+			<Typography variant="caption"><FontAwesomeIcon icon={faRobot}/> written by OpenAI</Typography>
+			<Typography sx={visuallyHidden}> (opens in a new window)</Typography>
+		</Link>
+	);
+}
