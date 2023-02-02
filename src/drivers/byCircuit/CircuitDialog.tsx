6bf92c1bf5805e02acf6221661a4ee8c5af9d0ab
Brett Harris <gtibrett@gmail.com>
2023-02-01 22:54:48 -0500

    enhanced driver data visualizations career performance circuit performance lap time swarm clustering by season better data mapping hooks

--- src/drivers/byCircuit/CircuitDialog.tsx
+++ src/drivers/byCircuit/CircuitDialog.tsx
@@ -1,6 +1,11 @@
-import {Box, Dialog, DialogContent, DialogTitle, Grid, Typography, useTheme} from '@mui/material';
+import {faTimes} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {Box, Dialog, DialogContent, DialogTitle, Grid, IconButton, Tooltip, Typography, useTheme} from '@mui/material';
 import CircuitMap from '../../circuits/CircuitMap';
 import {Driver} from '../../types/ergast';
+import Tabs from '../../ui-components/Tabs';
+import CircuitPerformance from '../analysis/CircuitPerformance';
+import LapTimesByYearSwarm from '../analysis/LapTimesByYearSwarm';
 import CircuitChart from './CircuitChart';
 import {CircuitWithRaces} from './Circuits';
 import CircuitTable from './CircuitTable';
@@ -19,21 +24,44 @@ export default function CircuitDialog({driver, circuit, onClose}: CircuitProps) 
 	return (
 		<Dialog open={true} onClose={onClose} maxWidth="lg" fullWidth>
 			<DialogTitle>
-				{circuit.circuitName}
-				<Typography paragraph variant="subtitle1">{driver.givenName} {driver.familyName}</Typography>
+				<Grid container spacing={2}>
+					<Grid item xs>
+						{circuit.circuitName}
+						<Typography paragraph variant="subtitle1">{driver.givenName} {driver.familyName}</Typography>
+					</Grid>
+					<Grid item>
+						<Tooltip title="Close" arrow placement="left">
+							<IconButton color="secondary" onClick={onClose}><FontAwesomeIcon fixedWidth icon={faTimes}/></IconButton>
+						</Tooltip>
+					</Grid>
+				</Grid>
 			</DialogTitle>
 			<DialogContent dividers sx={{height: '90vh'}}>
 				<Grid container spacing={2}>
 					<Grid item xs={9}>
-						<CircuitChart races={circuit.races}/>
+						<Tabs active="results" tabs={[
+							{
+								id: 'results',
+								label: 'Results',
+								content: (
+									<>
+										<CircuitChart races={circuit.races}/>
+										<CircuitTable races={circuit.races}/>
+									</>
+								)
+							},
+							{
+								id: 'laptimes',
+								label: 'Lap Times',
+								content: <LapTimesByYearSwarm driverId={driver.driverId} circuitId={circuit.circuitId}/>
+							}
+						]}/>
 					</Grid>
 					<Grid item xs={3}>
-						<Box sx={{border: `1px solid ${theme.palette.divider}`, borderRadius: theme.shape.borderRadius / 2, overflow: 'hidden'}}>
+						<Box sx={{border: `1px solid ${theme.palette.divider}`, borderRadius: theme.shape.borderRadius / 2, overflow: 'hidden', mb: 2}}>
 							<CircuitMap circuits={[circuit]} height={200} centerOn={circuit.Location} zoom/>
 						</Box>
-					</Grid>
-					<Grid item xs={12}>
-						<CircuitTable races={circuit.races}/>
+						<CircuitPerformance driverId={driver.driverId} circuitId={circuit.circuitId}/>
 					</Grid>
 				</Grid>
 			</DialogContent>
