38be53482ad2472bd07c9a0b5b992601a1d80e83
Brett Harris <gtibrett@gmail.com>
2023-05-01 11:59:17 -0400

    add Drivers page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,9 @@
-import {Box, Container, ThemeProvider} from '@mui/material';
+import {Box, Container, CssBaseline, SxProps, ThemeProvider} from '@mui/material';
 import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import ConstructorProvider from '../constructors/ConstructorProvider';
 import DriverProvider from '../drivers/DriverProvider';
-import {Driver, Home, Race} from '../pages';
+import {Driver, Drivers, Home, Race} from '../pages';
 import About from '../pages/About';
 import Circuit from '../pages/Circuit';
 import Constructor from '../pages/Constructor';
@@ -16,8 +16,24 @@ import GoogleAnalyticsProvider from './GoogleAnalyticsProvider';
 function Index() {
 	const theme = useEffTheme();
 	
+	const sx: SxProps = {
+		py: 2,
+		position: 'relative',
+		'&:before': {
+			position: 'absolute',
+			top: 0, left: 0,
+			zIndex: -1,
+			width: '100%',
+			height: '100%',
+			content: '" "',
+			backgroundImage: `url(${require('../ui-components/carbon-fiber-texture.png')})`,
+			opacity: theme.palette.mode === 'dark' ? .35 : 1
+		}
+	};
+	
 	return (
 		<AppStateProvider>
+			<CssBaseline/>
 			<ThemeProvider theme={theme}>
 				<BrowserRouter>
 					<GoogleAnalyticsProvider>
@@ -26,7 +42,7 @@ function Index() {
 								<Box sx={{position: 'fixed', overflow: 'auto', scrollbarColor: theme.palette.mode, top: 0, left: 0, right: 0, bottom: 0, background: theme.palette.background.default}}>
 									<Header/>
 									
-									<Container maxWidth="xl" component="main" sx={{py: 2}}>
+									<Container maxWidth="xl" component="main" sx={sx}>
 										<ErrorBoundary>
 											<Routes>
 												<Route
@@ -48,6 +64,10 @@ function Index() {
 												<Route
 													element={<Driver/>}
 													path="/driver/:id"
+												/>
+												<Route
+													element={<Drivers/>}
+													path="/drivers"
 												/>
 												<Route
 													element={<Race/>}
---
+++ src/pages/Drivers.tsx
@@ -0,0 +1,155 @@
+import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {Driver, Responses} from '@gtibrett/effone-hub-api';
+import {alpha, Button, Card, CardContent, CardHeader, Grid, Paper, Skeleton, TextField, TextFieldProps, Tooltip, Typography, useTheme} from '@mui/material';
+import {visuallyHidden} from '@mui/utils';
+import {DataGrid, GridColDef} from '@mui/x-data-grid';
+import {SyntheticEvent, useEffect, useState} from 'react';
+import Caxios from '../api/Caxios';
+import {getAPIUrl, mapDrivers} from '../api/Ergast';
+import {useAppState} from '../app/AppStateProvider';
+import ByLine from '../drivers/ByLine';
+import SeasonMenu from '../schedule/SeasonMenu';
+import Link from '../ui-components/Link';
+import Navigation from '../ui-components/Navigation';
+
+type DriversTableProps = {
+	drivers: Driver[];
+}
+
+function DriversTable({drivers}: DriversTableProps) {
+	
+	return (
+		<DataGrid
+			rows={drivers}
+			autoHeight
+			density="compact"
+			getRowId={r => r.driverId}
+			columns={
+				[
+					{
+						field:       'driver',
+						headerName:  'Driver',
+						flex:        1,
+						renderCell:  (({row}) => <ByLine id={row.driverId}/>),
+						valueGetter: (({row}) => `${row.familyName}, ${row.givenName}`)
+					}
+				] as GridColDef<Driver>[]
+			}
+			initialState={{
+				sorting: {
+					sortModel: [{field: 'driver', sort: 'asc'}]
+				}
+			}}
+		/>
+	);
+}
+
+export default function Drivers() {
+	const theme                           = useTheme();
+	const [{season: currentSeason}]       = useAppState();
+	const [drivers, setDrivers]           = useState<Driver[] | undefined>();
+	const [localFilters, setLocalFilters] = useState({
+		season: currentSeason,
+		search: ''
+	});
+	const [filters, setFilters]           = useState({
+		season: currentSeason,
+		search: ''
+	});
+	
+	const setSeason = (value: number) => {
+		setLocalFilters(cur => ({
+			...cur,
+			season: value
+		}));
+	};
+	
+	const setSearch: TextFieldProps['onChange'] = (ev) => {
+		setLocalFilters(cur => ({
+			...cur,
+			search: ev.target.value
+		}));
+	};
+	
+	const handleSearch = (ev: SyntheticEvent) => {
+		ev.preventDefault();
+		setFilters(localFilters);
+	};
+	
+	useEffect(() => {
+		if (Number(filters.season) > 0 || filters.search.length) {
+			let url = getAPIUrl(`/drivers.json`);
+			if (Number(filters.season) > 0) {
+				url = getAPIUrl(`/${filters.season}/drivers.json`);
+			}
+			
+			Caxios.get<Responses.DriversResponse>(url, {params: {limit: 2000}})
+			      .then(mapDrivers)
+			      .then(results => {
+				      if (!filters.search.length) {
+					      return results;
+				      }
+				      return results.filter(d => {
+					      const tokens = filters.search.toLowerCase().split(' ');
+					      for (const token of tokens) {
+						      if (d.givenName.toLowerCase().includes(token) || d.familyName.toLowerCase().includes(token)) {
+							      return true;
+						      }
+					      }
+					      return false;
+				      });
+			      })
+			      .then(results => setDrivers(results))
+			      .catch((err) => {
+				      console.log(err);
+				      setDrivers([]);
+			      });
+		}
+	}, [filters.season, filters.search]);
+	
+	let content = <Skeleton variant="rectangular" height={400}/>;
+	if (drivers) {
+		content = <>
+			<Paper elevation={0} sx={{p: 2, boxShadow: `inset 8px 0 0 ${alpha(theme.palette.secondary.main, .5)}`, border: `1px solid ${theme.palette.secondary.main}`}}>
+				<form onSubmit={handleSearch}>
+					<Grid container spacing={1}>
+						<Grid item xs>
+							<TextField fullWidth size="small" InputLabelProps={{shrink: true}} id="drivers-search-filter" label="Driver" variant="outlined" value={localFilters.search} onChange={setSearch}/>
+						</Grid>
+						<Grid item xs>
+							<SeasonMenu required={false} variant="normal" id="drivers-season-filter" season={localFilters.season} setSeason={setSeason}/>
+						</Grid>
+						<Grid item>
+							<Tooltip title="Search" arrow placement="bottom">
+								<Button color="secondary" type="submit" variant="contained" onClick={handleSearch}><FontAwesomeIcon icon={faMagnifyingGlass} style={{fontSize: 26}}/><Typography sx={visuallyHidden}>Search</Typography></Button>
+							</Tooltip>
+						</Grid>
+					</Grid>
+				</form>
+			</Paper>
+			<DriversTable drivers={drivers}/>
+		</>;
+	}
+	
+	return (
+		<Grid container spacing={2}>
+			<Grid item xs={12}>
+				<Navigation>
+					<Link to="/">{currentSeason} Season</Link>
+					<Typography>All Drivers</Typography>
+				</Navigation>
+			</Grid>
+			
+			<Grid item xs={12}>
+				<Card elevation={0}>
+					<CardHeader title="All Drivers"/>
+					
+					<CardContent>
+						{content}
+					</CardContent>
+				</Card>
+			</Grid>
+		</Grid>
+	);
+}
