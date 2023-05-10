d7f4db181e902caa0bc395899b8143ca48372d38
Brett Harris <gtibrett@gmail.com>
2023-05-10 19:11:37 -0400

    `to` prop should be optional

--- src/ui-components/Link.tsx
+++ src/ui-components/Link.tsx
@@ -2,9 +2,11 @@ import {Link as MuiLink, LinkProps as MuiLinkProps, useTheme} from '@mui/materia
 import {FC} from 'react';
 import {Link as RouterLink, LinkProps} from 'react-router-dom';
 
-type SmartLinkProps = Omit<MuiLinkProps, 'component'> & LinkProps;
+type SmartLinkProps = Omit<MuiLinkProps, 'component'> & Omit<LinkProps, 'to'> & {
+	to?: LinkProps['to']
+};
 
-const Link: FC<SmartLinkProps> = ({sx = {}, color = 'secondary', ...props}) => {
+const Link: FC<SmartLinkProps> = ({sx = {}, to, color = 'secondary', ...props}) => {
 	const theme          = useTheme();
 	const selectectColor = (() => {
 		switch (color) {
@@ -21,18 +23,17 @@ const Link: FC<SmartLinkProps> = ({sx = {}, color = 'secondary', ...props}) => {
 	const a11ySx = {
 		...sx,
 		...(selectectColor ? ({
-			px: .25,
+			px:                 .25,
 			'&:hover, &:focus': {
 				background: selectectColor.main,
-				color: theme.palette.background.paper
+				color:      theme.palette.background.paper
 			}
 		}) : {})
 	};
 	
-	if (props.to) {
-		return <MuiLink component={RouterLink} color={color} {...props} sx={a11ySx}/>;
-	}
-	else {
+	if (to) {
+		return <MuiLink component={RouterLink} color={color} to={to} {...props} sx={a11ySx}/>;
+	} else {
 		return <MuiLink color={color} {...props} sx={a11ySx}/>;
 	}
 	
