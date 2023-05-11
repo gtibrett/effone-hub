54c5bb93f1179c274e990d353f55ba6e395a5236
Brett Harris <gtibrett@gmail.com>
2023-05-10 22:09:21 -0400

    about page refactor

---
+++ src/about/Repositories.tsx
@@ -0,0 +1,31 @@
+import {faGithub} from '@fortawesome/free-brands-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {Card, CardContent, CardHeader, List, ListItem, ListItemIcon, ListItemText} from '@mui/material';
+import Link from '../ui-components/Link';
+import OpensInNewWindow from '../ui-components/OpensInNewWindow';
+
+const Repositories = () => (
+	<Card sx={{height: '100%'}}>
+		<CardHeader title="Repositories"/>
+		<CardContent>
+			<List>
+				<ListItem>
+					<ListItemIcon><FontAwesomeIcon icon={faGithub}/></ListItemIcon>
+					<ListItemText
+						primary={<Link href="https://github.com/gtibrett/effOneHub">effOneHub <OpensInNewWindow/></Link>}
+						secondary="This site. Go look under the engine cover."
+					/>
+				</ListItem>
+				<ListItem>
+					<ListItemIcon><FontAwesomeIcon icon={faGithub}/></ListItemIcon>
+					<ListItemText
+						primary={<Link href="https://github.com/gtibrett/effone-hub-api">effone-hub-api <OpensInNewWindow/></Link>}
+						secondary="A nodejs implementation of the Ergast API. Still a work in progress."
+					/>
+				</ListItem>
+			</List>
+		</CardContent>
+	</Card>
+);
+
+export default Repositories;
