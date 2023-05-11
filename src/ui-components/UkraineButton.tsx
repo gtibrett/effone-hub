b328c1a6352ec3453f3becd7b312a71e8413dee0
Brett Harris <gtibrett@gmail.com>
2023-05-10 22:08:57 -0400

    a11y fixes

--- src/ui-components/UkraineButton.tsx
+++ src/ui-components/UkraineButton.tsx
@@ -5,12 +5,15 @@ export default function UkraineButton() {
 	const theme = useTheme();
 	
 	return (
-		<Box sx={{position: 'fixed', bottom: 0, right: 0, pr: 4, pb: 3}}>
+		<Box sx={{position: 'fixed', bottom: 0, right: 0, pr: 4, pb: 3, zIndex: theme.zIndex.fab + 1}}>
 			<Tooltip title={<>We stand with Ukraine and its people.<br/>Show your support.</>} placement="left" arrow>
-				<Fab href="https://cottonbureau.com/p/SE43Q8/shirt/stand-with-ukraine" target="_blank"
+				<Fab
+					aria-label="Stand with Ukraine"
+					href="https://cottonbureau.com/p/SE43Q8/shirt/stand-with-ukraine"
+					target="_blank"
 					sx={{
 						background: 'linear-gradient(0deg, #F7D748 0%, #F7D748 50%, #255AB5 50%, #255AB5 100%)',
-						border: `1px solid ${theme.palette.common.white}`
+						border:     `1px solid ${theme.palette.common.white}`
 					}}>
 				</Fab>
 			</Tooltip>
