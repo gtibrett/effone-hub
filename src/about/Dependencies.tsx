dad6ffa27ec90a1cd8da044b8167e384aeb8b1ad
Brett Harris <gtibrett@gmail.com>
2023-09-16 17:25:37 -0400

    update dependencies

--- src/about/Dependencies.tsx
+++ src/about/Dependencies.tsx
@@ -1,7 +1,7 @@
+import {Link} from '@gtibrett/mui-additions';
 import {Card, CardContent, CardHeader, List, ListItem, ListItemText, ListSubheader} from '@mui/material';
 import {Fragment} from 'react';
 import {OpensInNewWindow} from '../ui-components';
-import {Link} from '@gtibrett/mui-additions';
 
 type DependencyCategory = {
 	title: string;
