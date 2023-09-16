8e9a0f4ed2c9ca8b26acce4029eb4c86c84b83fa
Brett Harris <gtibrett@gmail.com>
2023-09-16 17:27:42 -0400

    remove obsolete files

--- src/api/getCircuitDescription.ts
+++
@@ -1,7 +0,0 @@
-import {Circuit} from '@gtibrett/effone-hub-api';
-
-const data = require(`./circuits.json`);
-
-export default function getCircuitDescription(circuitId: Circuit['circuitId']): string | undefined {
-	return data[circuitId];
-}
