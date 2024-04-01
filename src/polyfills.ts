278bc28ce5d78c2a658867b77f1bca1b94e808d9
Brett Harris <gtibrett@gmail.com>
2024-04-01 00:29:52 -0400

    add a polyfill for Array to remove duplicates

---
+++ src/polyfills.ts
@@ -0,0 +1,19 @@
+declare global {
+	interface Array<T> {
+		removeDuplicates: (comparator?: (a: T, b: T) => boolean) => T[];
+	}
+}
+
+// eslint-disable-next-line no-extend-native
+Array.prototype.removeDuplicates = function (comparator = (a, b) => a === b) {
+	return this.reduce((prev, cur) => {
+		if (!prev.find((val: typeof this[number]) => comparator(val, cur))) {
+			prev.push(cur);
+		}
+		return prev;
+	}, []);
+};
+
+// necessary for declare global above
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {};
