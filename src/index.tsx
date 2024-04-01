4f18b6e178662d247bfe3e746569108915cbbfba
Brett Harris <gtibrett@gmail.com>
2024-04-01 00:29:30 -0400

    font updates

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,9 @@
+import '@fontsource/anton';
+import '@fontsource/roboto';
+import '@fontsource/roboto-mono';
+import '@fontsource/racing-sans-one';
+import '@fontsource/titillium-web';
+import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './app/App';
 
