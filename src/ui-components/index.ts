4ebe5b1d6a650847c4ecdd0f6888ca72b297d288
Brett Harris <gtibrett@gmail.com>
2024-04-08 15:38:06 -0400

    reorg files

--- src/ui-components/index.ts
+++ src/ui-components/index.ts
@@ -1,13 +1,10 @@
 export type {DataWithValue, StatFormatter} from './stats';
 export type {ListFiltersProps} from './TableFilter';
-export {default as OpenAILink} from './citations/OpenAILink';
-export {default as WikipediaLink} from './citations/WikipediaLink';
+export * from './citations';
 export {default as OpensInNewWindow} from './OpensInNewWindow';
 export {default as Page} from './Page';
-export {default as PropertiesTable} from './PropertiesTable';
-export {default as PropertiesTableRow} from './PropertiesTableRow';
-export {default as StatCard} from './stats/StatCard';
-export {default as Stats} from './stats/Stats';
+export * from './propertiesTable';
+export * from './stats';
 export {default as TableFilter, filterByFreeformText, setStringFilter, filterByNumber} from './TableFilter';
 export {useEffTheme, useDarkMode, useInvertedTheme} from './Theme';
 export {NivoTooltipFactory, useNivoTheme, useGetAccessibleChartColors} from './nivo';
