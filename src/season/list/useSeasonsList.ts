2413e330c40774013c6fe447783c8ed915c684b3
Brett Harris <gtibrett@gmail.com>
2024-04-08 19:37:12 -0400

    no need to memoize something that has no cost to calculate

--- src/season/list/useSeasonsList.ts
+++ src/season/list/useSeasonsList.ts
@@ -1,14 +1,7 @@
 import {useQuery} from '@apollo/client';
-import {useMemo} from 'react';
 import SeasonsQuery from './SeasonsQuery';
 import {Data} from './types';
 
 export default function useSeasonsList() {
-	const {loading, data} = useQuery<Data>(SeasonsQuery);
-	
-	return useMemo(() => {
-		const seasons = data?.seasons?.filter(s => s.racesByYear[0].driverStandings.length) || [];
-		
-		return {data: seasons, loading};
-	}, [data?.seasons, loading]);
+	return useQuery<Data>(SeasonsQuery);
 }
