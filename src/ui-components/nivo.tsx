6f81859dc28a8f8e66e90221b5388d2a4c18336a
Brett Harris <gtibrett@gmail.com>
2024-04-08 15:36:06 -0400

    theme improvements

--- src/ui-components/nivo.tsx
+++ src/ui-components/nivo.tsx
@@ -19,115 +19,115 @@ type NivoTheme = Theme & {
 }
 
 export const useNivoTheme = (): NivoTheme => {
-	const theme           = useTheme();
-	const invertedTheme   = useInvertedTheme();
-	const captionFontSize = 11;
+	const theme         = useTheme();
+	const invertedTheme = useInvertedTheme();
 	
 	return {
-		'background':  alpha(theme.palette.background.default, .25),
-		text:          {
-			color:        theme.palette.text.primary,
-			'fontSize':   captionFontSize,
-			'fontFamily': "'Titillium Web', sans-serif"
+		background:  alpha(theme.palette.background.default, .25),
+		text:        {
+			color:      theme.palette.text.primary,
+			fontSize:   theme.typography.caption.fontSize,
+			fontFamily: "'Titillium Web', sans-serif"
 		},
-		translation:   {},
-		'axis':        {
-			'domain': {
-				'line': {
-					'stroke':      theme.palette.divider,
-					'strokeWidth': 1
+		translation: {},
+		axis:        {
+			domain: {
+				line: {
+					stroke:      theme.palette.divider,
+					strokeWidth: 1
 				}
 			},
-			'legend': {
-				'text': {
-					'fontSize': captionFontSize,
-					'fill':     theme.palette.text.secondary
+			legend: {
+				text: {
+					fontSize: theme.typography.caption.fontSize,
+					fill:     theme.palette.text.secondary
 				}
 			},
-			'ticks':  {
-				'line': {
-					'stroke':      theme.palette.divider,
-					'strokeWidth': 1
+			ticks:  {
+				line: {
+					stroke:      theme.palette.divider,
+					strokeWidth: 1
 				},
-				'text': {
-					'fontSize': captionFontSize,
-					'fill':     theme.palette.text.secondary
+				text: {
+					fontSize: theme.typography.caption.fontSize,
+					fill:     theme.palette.text.secondary
 				}
 			}
 		},
-		'grid':        {
-			'line': {
-				'stroke':      theme.palette.divider,
-				'strokeWidth': 1
+		grid:        {
+			line: {
+				stroke:      theme.palette.divider,
+				strokeWidth: 1
 			}
 		},
-		'legends':     {
-			'title': {
-				'text': {
-					'fontSize': captionFontSize,
-					'fill':     theme.palette.text.secondary
+		legends:     {
+			title: {
+				text: {
+					fontSize: theme.typography.caption.fontSize,
+					fill:     theme.palette.text.secondary
 				}
 			},
-			'text':  {
-				'fontSize': captionFontSize,
-				'fill':     theme.palette.text.secondary
+			text:  {
+				fontSize: theme.typography.caption.fontSize,
+				fill:     theme.palette.text.secondary
 			},
-			'ticks': {
-				'line': {},
-				'text': {
-					'fontSize': captionFontSize,
-					'fill':     theme.palette.text.secondary
+			ticks: {
+				line: {},
+				text: {
+					fontSize: theme.typography.caption.fontSize,
+					fill:     theme.palette.text.secondary
 				}
 			}
 		},
-		'annotations': {
-			'text':    {
-				'fontSize':       captionFontSize,
-				'fill':           theme.palette.text.secondary,
-				'outlineWidth':   2,
-				'outlineColor':   theme.palette.background.paper,
-				'outlineOpacity': 1
+		annotations: {
+			text:    {
+				fontSize:       theme.typography.caption.fontSize,
+				fill:           theme.palette.text.secondary,
+				outlineWidth:   2,
+				outlineColor:   theme.palette.background.paper,
+				outlineOpacity: 1
 			},
-			'link':    {
-				'stroke':         theme.palette.text.primary,
-				'strokeWidth':    1,
-				'outlineWidth':   2,
-				'outlineColor':   theme.palette.background.paper,
-				'outlineOpacity': 1
+			link:    {
+				stroke:         theme.palette.text.primary,
+				strokeWidth:    1,
+				outlineWidth:   2,
+				outlineColor:   theme.palette.background.paper,
+				outlineOpacity: 1
 			},
-			'outline': {
-				'stroke':         theme.palette.text.primary,
-				'strokeWidth':    2,
-				'outlineWidth':   2,
-				'outlineColor':   theme.palette.background.paper,
-				'outlineOpacity': 1
+			outline: {
+				stroke:         theme.palette.text.primary,
+				strokeWidth:    2,
+				outlineWidth:   2,
+				outlineColor:   theme.palette.background.paper,
+				outlineOpacity: 1
 			},
-			'symbol':  {
-				'fill':           theme.palette.text.primary,
-				'outlineWidth':   2,
-				'outlineColor':   theme.palette.background.paper,
-				'outlineOpacity': 1
+			symbol:  {
+				fill:           theme.palette.text.primary,
+				outlineWidth:   2,
+				outlineColor:   theme.palette.background.paper,
+				outlineOpacity: 1
 			}
 		},
 		
 		tooltip: {
+			//move this to wrapper: {} when it becomes available
 			// @ts-ignore
-			backdropFilter:   'blur(4px)',
-			padding:          0,
-			borderRadius:     theme.spacing(.5),
-			overflow:         'hidden',
-			'container':      {
-				'background': invertedTheme.palette.background.paper,
-				'color':      invertedTheme.palette.getContrastText(invertedTheme.palette.background.paper),
-				'fontSize':   captionFontSize
+			backdropFilter: 'blur(4px)',
+			
+			padding:        0,
+			borderRadius:   invertedTheme.spacing(.5),
+			overflow:       'hidden',
+			container:      {
+				background: invertedTheme.palette.background.paper,
+				color:      invertedTheme.palette.getContrastText(invertedTheme.palette.background.paper),
+				fontSize:   invertedTheme.typography.caption.fontSize
 			},
-			'basic':          {},
-			'chip':           {},
-			'table':          {},
-			'tableCell':      {},
-			'tableCellValue': {}
+			basic:          {},
+			chip:           {},
+			table:          {},
+			tableCell:      {},
+			tableCellValue: {}
 		}
-		
 	};
 };
 
