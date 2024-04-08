80d695ed7fbcd990c607eacae9b2f34e402dbf83
Brett Harris <gtibrett@gmail.com>
2024-04-08 19:38:49 -0400

    improved chart design and performance

--- src/constructor/season/SeasonChart.tsx
+++ src/constructor/season/SeasonChart.tsx
@@ -1,41 +1,47 @@
 import {QueryResult} from '@apollo/client/react/types/types';
-import {useGetAccessibleChartColors, useNivoTheme} from '@effonehub/ui-components';
-import {Box, Skeleton, useTheme} from '@mui/material';
+import {useGetTeamColor} from '@effonehub/constructor';
+import {useNivoTheme} from '@effonehub/ui-components';
+import {alpha, Box, Skeleton} from '@mui/material';
 import {ResponsiveLine, Serie as LineSerie} from '@nivo/line';
 import {ConstructorPageData} from '../types';
 
 type SeasonChartProps = Pick<QueryResult<ConstructorPageData>, 'data' | 'loading'> & { season: number };
 
 export default function SeasonChart({data, loading}: SeasonChartProps) {
-	const theme                    = useTheme();
-	const nivoTheme                = useNivoTheme();
-	const getAccessibleChartColors = useGetAccessibleChartColors();
+	const nivoTheme    = useNivoTheme();
+	const getTeamColor = useGetTeamColor();
+	let maxPosition    = 20;
 	
 	if (loading || !data) {
 		return <Skeleton variant="rectangular" height={132}/>;
 	}
 	
-	const colors               = getAccessibleChartColors(data.team.colors.primary || theme.palette.primary.main);
-	const drivers: LineSerie[] = [];
+	const colors    = [
+		getTeamColor(data.team.colors, 'primary', false),
+		getTeamColor(data.team.colors, 'secondary', false),
+		alpha(getTeamColor(data.team.colors, 'primary', false), .75),
+		alpha(getTeamColor(data.team.colors, 'secondary', false), .75)
+	];
+	const rounds    = Math.max(...(data?.team.results.map(rs => rs.race?.round || 0)));
+	const blankData = (new Array<number>(rounds)).fill(0).map((v, i) => ({x: i + 1, y: null}));
 	
-	data.team.results.forEach(result => {
-		let index = drivers.findIndex(d => d.id === result.driver.code);
-		
-		if (index === -1) {
-			drivers.push({
-				id:   String(result.driver.code),
-				data: []
-			});
-			
-			index = drivers.length - 1;
-		}
-		
-		// @ts-ignore
-		drivers[index].data.push({
-			x: result.race.round,
-			y: result.positionOrder
-		});
-	});
+	const drivers: LineSerie[] =
+		      data.team.results
+		          .map(r => String(r.driver?.code))
+		          .removeDuplicates()
+		          .map(id => ({
+			          id,
+			          data: blankData.map(d => {
+				          const point = {
+					          x: d.x,
+					          y: data.team.results.find(rs => String(rs.driver?.code) === id && rs.race?.round === d.x)?.positionOrder || null
+				          };
+				          
+				          maxPosition = Math.max(maxPosition, point.y || 0);
+				          
+				          return point;
+			          })
+		          }));
 	
 	return (
 		<Box sx={{height: 132, width: '100%'}} aria-hidden>
@@ -43,9 +49,11 @@ export default function SeasonChart({data, loading}: SeasonChartProps) {
 				theme={nivoTheme}
 				data={drivers}
 				colors={colors}
+				lineWidth={4}
+				pointSize={12}
 				yScale={{
 					type: 'linear',
-					min:  20,
+					min:  maxPosition,
 					max:  1
 				}}
 				axisLeft={null}
@@ -59,7 +67,7 @@ export default function SeasonChart({data, loading}: SeasonChartProps) {
 				axisBottom={null}
 				enableGridX={false}
 				gridYValues={[1, 5, 10, 15, 20]}
-				margin={{top: 5, left: 5, right: 25, bottom: 32}}
+				margin={{top: 24, right: 36, bottom: 32, left: 16}}
 				legends={[
 					{
 						anchor:        'bottom',
