a5a3dd9458130ff392fbfd574f489a5345ea9b58
Brett Harris <gtibrett@gmail.com>
2024-04-10 13:09:11 -0400

    improved error handling and styling

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,29 +7,22 @@ import useApolloClient from '../useApolloClient';
 import AppStateProvider from './AppStateProvider';
 import Routes from './Routes';
 
-const ThemedApp = () => {
-	const theme = useEffTheme();
-	
-	return (
-		<ThemeProvider theme={theme}>
-			<Routes/>
-		</ThemeProvider>
-	);
-};
-
 export default function App() {
+	const theme    = useEffTheme();
 	const {client} = useApolloClient();
 	
 	return (
 		<ApolloProvider client={client}>
-			<AppStateProvider>
-				<BrowserRouter>
-					<GoogleAnalyticsProvider trackingId={process.env.REACT_APP_GA_TRACKING_ID}>
-						<CssBaseline/>
-						<ThemedApp/>
-					</GoogleAnalyticsProvider>
-				</BrowserRouter>
-			</AppStateProvider>
+			<CssBaseline/>
+			<ThemeProvider theme={theme}>
+				<AppStateProvider>
+					<BrowserRouter>
+						<GoogleAnalyticsProvider trackingId={process.env.REACT_APP_GA_TRACKING_ID}>
+							<Routes/>
+						</GoogleAnalyticsProvider>
+					</BrowserRouter>
+				</AppStateProvider>
+			</ThemeProvider>
 		</ApolloProvider>
 	);
 }
